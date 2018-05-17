3f93c247328392152127a21e13516f10f08e24ea
fadyl bello <bellor_f16@myges.fr>
2018-05-17 11:49:13 +0200

    Merge Master

--- src/app/pages/capsule/page-capsule.component.ts
+++ src/app/pages/capsule/page-capsule.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SpacexApiService } from '../backend/spacex-api.service';
+import { SpacexApiService } from '../../backend/spacex-api.service';
 
 @Component({
   selector: 'app-page-capsule',
